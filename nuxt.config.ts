348b756539ec2468e93f244b17ee10dad4748814
zsj <709196364@qq.com>
2024-08-21 11:07:32 +0800

    修改baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     ssr: false,
     devtools: { enabled: true },
+    app: { baseURL: "/MapleStory_Simulator" },
     modules: ["@nuxtjs/tailwindcss", "@element-plus/nuxt", "@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt"],
     elementPlus: {
         /** Options */
--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,18 @@
             <el-card shadow="never">
                 <!-- <img src="/images/simulator-icon.gif" alt="纸娃娃" /> -->
                 <el-row class="icon-container" align="middle">
-                    <el-image src="/images/simulator-icon.gif" fit="contain" />
+                    <!-- <el-image src="/images/simulator-icon.gif" fit="contain" /> -->
+                    <img src="/images/simulator-icon.gif" />
                 </el-row>
                 <div class="text-14 text-center font-bold mt-10">纸娃娃V1</div>
             </el-card>
         </NuxtLink>
 
-        <div class="ml-20">
+        <!-- <div class="ml-20">
             <el-link href="https://element-plus.org/" type="primary" target="_blank">Element Plus</el-link>
             <br />
             <el-link href="/_tailwind/" type="primary" target="_blank">Tailwind Config Viewer</el-link>
-        </div>
+        </div> -->
     </el-row>
 </template>
 
